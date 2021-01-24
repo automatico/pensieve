4b2a67946495e645d269b8b03deb8cf01f986ed4
8radm1n <bradleysearle@gmail.com>
2021-01-24 22:45:11 +1000

    docker

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM elixir:1.11.3
+
+RUN mix local.hex --force 
+
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pensieve.MixProject do
   def project do
     [
       app: :pensieve,
-      version: "2021.01.01",
+      version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
