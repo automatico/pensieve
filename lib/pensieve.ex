0edcfd8d079c5738230c4656e83caa6ab2d4fd88
8radm1n <bradleysearle@gmail.com>
2021-01-24 22:23:07 +1000

    outline module

--- lib/pensieve.ex
+++ lib/pensieve.ex
@@ -4,15 +4,12 @@ defmodule Pensieve do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pensieve.hello()
-      :world
-
   """
-  def hello do
-    :world
+  :ssh.start()
+  def connect do
+    {:ok, conn} = SSHEx.connect ip: '#', user: '#', password: '#'
+    {:ok, stdout, stderr, 2} = SSHEx.run conn, '...', separate_streams: true
+    IO.puts "stdout: #{stdout}" 
+    IO.puts "stderr: #{stderr}"
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pensieve.MixProject do
   def project do
     [
       app: :pensieve,
-      version: "0.1.0",
+      version: "2021.01.01",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +21,9 @@ defmodule Pensieve.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:sshex, "~> 2.1"},
+      {:ex_doc, "~> 0.23"},
+      {:earmark, "~> 1.4"}
     ]
   end
 end
