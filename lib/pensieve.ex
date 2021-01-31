2e9799f830cc0df5a872c09394893f7ac3c730f8
Brad Searle <bradleysearle@gmail.com>
2021-01-31 19:45:31 +1000

    algorithms

--- lib/pensieve.ex
+++ lib/pensieve.ex
@@ -8,44 +8,30 @@ defmodule Pensieve do
   @doc """
   """
   def connect do
-  
-    # Cisco IOS
-    # :ssh.start()
-    # ssh_opts = :ssh.chk_algos_opts([{:modify_algorithms, [{:append, [{:kex, [:"diffie-hellman-group-exchange-sha1"]}]}]}])
-
-
-    :ssh.chk_algos_opts(
-            [{:modify_algorithms,
-            [{:prepend,
-                [{:kex, ['diffie-hellman-group1-sha1']}]
-          }
-            ]
-              }
-            ])
 
     commands = %{
       juniper: [
-        'set cli screen-length 0',
-        'set cli screen-width 0',
-        'show system uptime',
-        'show interfaces terse',
+        # 'set cli screen-length 0',
+        # 'set cli screen-width 0',
+        # 'show system uptime',
+        # 'show interfaces terse',
         'show configuration | display set'
       ],
       arista: [
-        'terminal length 0',
-        'enable',
-        'show clock',
-        'show version',
-        'show ip int brie',
+        # 'terminal length 0',
+        # 'enable',
+        # 'show clock',
+        # 'show version',
+        # 'show ip int brie',
         'show running-config'
       ],
       cisco: [
         # 'terminal length 0',
         # 'enable',
-        'show clock',
+        # "show clock",
         # 'show version',
         # 'show ip int brie',
-        # 'show running-config'
+        'show running-config'
       ]
     }
 
@@ -57,33 +43,38 @@ defmodule Pensieve do
         ssh_user: "admin",
         ssh_pass: "cisco",
       },
-      # %{
-      #   ip: "192.168.255.151",
-      #   hostname: "vmx",
-      #   vendor: :juniper,
-      #   ssh_user: "admin",
-      #   ssh_pass: "Juniper",
-      # },
-      # %{
-      #   ip: "192.168.255.152",
-      #   hostname: "veos",
-      #   vendor: :arista,
-      #   ssh_user: "admin",
-      #   ssh_pass: "arista",
-      # },
+      %{
+        ip: "192.168.255.151",
+        hostname: "vmx",
+        vendor: :juniper,
+        ssh_user: "admin",
+        ssh_pass: "Juniper",
+      },
+      %{
+        ip: "192.168.255.152",
+        hostname: "veos",
+        vendor: :arista,
+        ssh_user: "admin",
+        ssh_pass: "arista",
+      },
     ]
 
     Enum.each(devices, fn(device) -> 
 
       File.rm("#{device.hostname}.txt")
 
-      {:ok, conn} = SSHEx.connect(ip: device.ip, user: device.ssh_user, password: device.ssh_pass, modify_algorithms: [{:append, [{:kex, [:"diffie-hellman-group-exchange-sha1"]}]}])
+      {:ok, conn} = SSHEx.connect(
+        ip: device.ip, 
+        user: device.ssh_user, 
+        password: device.ssh_pass, 
+        modify_algorithms: [
+          {:append, [{:kex, [:"diffie-hellman-group-exchange-sha1"]}]}
+          ]
+        )
 
       Enum.each(commands[device.vendor], fn(command) -> 
         send_command(conn, command, device.hostname)
       end)
-      # res = SSHEx.cmd!(conn, 'show clock')
-      # IO.puts(res)
 
     end)
 
@@ -106,9 +97,9 @@ defmodule Pensieve do
   end
 
   defp process_result(type, result, hostname) do
-    # cwd = File.cwd!()
-    # File.write("#{cwd}/#{hostname}.txt", result, [:append])
-    IO.puts("#{hostname}: #{type}: #{result}")
+    cwd = File.cwd!()
+    File.write("#{cwd}/#{hostname}.txt", result, [:append])
+    # IO.puts("#{hostname}: #{type}: #{result}")
   end
 
 end
